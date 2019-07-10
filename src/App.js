cf48b50cd6643c43d29294049701698da417c6c3
Alan Fidelino <afidedev26@gmail.com>
2019-07-10 21:50:40 +1000

    Add Card component to render individual cards

--- src/App.js
+++ src/App.js
@@ -17,13 +17,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters } = this.state;
     return (
       <div>
-        <CardList>
-          {this.state.monsters.map(monster => (
-            <h1 key={monster._id}>{monster.name}</h1>
-          ))}
-        </CardList>
+        <CardList monsters={monsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./card-list.styles.css";
+import Card from "../card/card.component";
 
-const CardList = props => {
-  return <div className="card-list">{props.children}</div>;
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map(monster => (
+        <Card key={monster._id}>
+          <h1>{monster.name}</h1>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default CardList;
