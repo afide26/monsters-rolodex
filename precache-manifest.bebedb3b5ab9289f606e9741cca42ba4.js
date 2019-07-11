7eacdf967e5cef131f161763d4022ba607eb8552
Alan Fidelino <afidedev26@gmail.com>
2019-07-11 12:41:24 +1000

    Updates

---
+++ precache-manifest.bebedb3b5ab9289f606e9741cca42ba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28463a26b8944973831b39d744f204e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "98505a590ee79b4cc806",
+    "url": "/monsters-rolodex/static/css/main.8897fe79.chunk.css"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/monsters-rolodex/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "98505a590ee79b4cc806",
+    "url": "/monsters-rolodex/static/js/main.2f7085c6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
