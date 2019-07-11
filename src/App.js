6ea69af8f4da064549bc139f0f04fc3dcc0a91de
Alan Fidelino <afidedev26@gmail.com>
2019-07-11 11:59:42 +1000

    Implement search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import "./components/card-list/card-list.styles.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -16,11 +18,23 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
+
+  handleOnChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
-    const { monsters } = this.state;
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
-      <div>
-        <CardList monsters={monsters} />
+      <div className="App">
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={e => this.handleOnChange(e)}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
